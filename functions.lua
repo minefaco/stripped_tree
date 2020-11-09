c90b303ba1b843c15e7fc2a02c89be391197b201
minefaco <63024226+minefaco@users.noreply.github.com>
2020-11-09 09:11:41 -0500

    Support for moretrees added!

--- functions.lua
+++ functions.lua
@@ -49,8 +49,6 @@ function chisel_tree.register_axes(mod_name,axe_types)
                             local old_node = minetest.get_node(pos)
                             minetest.swap_node(pos, {name = mod_name..":".."stripped_"..node_name, param2 = old_node.param2})
                             itemstack:add_wear(65535 / 299) -- 300 uses
-                            minetest.chat_send_all(mod_name)
-                            minetest.chat_send_all(node_name)
 
 		            if not creative_mode then
                                 local inv = user:get_inventory()
