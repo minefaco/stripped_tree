5a9ed6f2834437d0f64b2893d949f10de19ef52b
minefaco <63024226+minefaco@users.noreply.github.com>
2020-11-18 19:53:39 -0500

    Add files via upload

---
+++ ethereal.lua
@@ -0,0 +1,23 @@
+--Register stripped trees
+local mod_name = "ethereal"
+local trunk_names = {
+    "banana_trunk",
+    "birch_trunk",
+    "scorched_tree",
+    "yellow_trunk",
+    "willow_trunk",
+    "redwood_trunk",
+    "sakura_trunk",
+    "frost_tree",
+    "palm_trunk",
+}
+
+stripped_tree.register_trunk(mod_name,trunk_names)
+
+
+--Register axes
+local axe_types = {
+"axe_crystal",
+}
+
+stripped_tree.register_axes(mod_name,axe_types)
