553d4e90c47f4704f457b06638d52818dc519d69
Christian Sirolli <34974905+HeyITGuyFixIt@users.noreply.github.com>
2022-11-27 12:20:59 -0500

    Fixing #18
    Needs testing. But should fix #18

--- functions.lua
+++ functions.lua
@@ -14,6 +14,8 @@ end
 --Function to swap nodes
 stripped_tree.swap_node = function(pos,user,creative_mode)
     local old_node = minetest.get_node(pos)
+    local old_node_name = old_node.name or pos
+    local mod_name, node_name = unpack(old_node_name:split(":"))
     local stripped =mod_name..":".."stripped_"..node_name
     minetest.swap_node(pos,{name=stripped,param2=old_node.param2})
     --itemstack:add_wear(65535 / 299) this is not useful at moment.
